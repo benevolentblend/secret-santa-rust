d64c03f0e7d55f47d92999d7a760026dfe84c8a3
Ben Thomas <benjamin@thomasnetwork.net>
2020-08-18 10:38:49 -0400

    removed use of player id

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,31 @@ use std::fmt;
 use std::collections::HashMap;
 
 struct Player {
-    id: u32,
     name: &'static str,
     group_id: u32
 }
 
 impl fmt::Display for Player {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Player: {} ({}) - {}", self.name, self.id, self.group_id)
+        write!(f, "Player: {} ({})", self.name, self.group_id)
     }
-}
-
-fn get_player_from_id(players:&Vec<Player>, player_id: u32) -> Option<usize> {
-
-    for (i, player) in players.iter().enumerate() {
-        if player.id == player_id {
-            return Some(i);
-        }
-    }
-    None
 }
 
 fn get_available_player_pairings(
     players: &Vec<Player>,
-    matchings: &HashMap<u32, u32>,
-    current_group: u32) -> Vec<u32> {
+    matchings: &HashMap<usize, usize>,
+    current_group: u32) -> Vec<usize> {
     let mut pairings = Vec::new();
 
-    for player in players {
-        print!("{}", player);
+    for (i, player) in players.iter().enumerate() {
         if player.group_id == current_group {
-            print!(" Same Group\n");
             continue;
         }
-        match matchings.get(&player.id) { 
+        match matchings.get(&i) { 
             None => {
-                pairings.push(player.id);
+                pairings.push(i);
             },
-            Some(i) => {
-                print!(" {}\n", i);
+            Some(_) => {
                 continue;
             }
         }
@@ -55,10 +41,10 @@ fn get_available_player_pairings(
 
 fn match_players(
     players: &mut Vec<Player>,
-    matchings: &mut HashMap<u32, u32>,
+    matchings: &mut HashMap<usize, usize>,
     current_player: usize) -> bool {
 
-    let Player {id, group_id, ..} = players[current_player];
+    let Player {group_id, ..} = players[current_player];
     let available_pairings = get_available_player_pairings(players, matchings, group_id);
 
     if available_pairings.len() == 0 {
@@ -73,7 +59,7 @@ fn match_players(
     for available_pair in available_pairings {
 
         // println!("Inserting {} as {}", available_pair, id);
-        matchings.insert(available_pair, id);
+        matchings.insert(available_pair, current_player);
 
         if current_player + 1 == players.len() {
             return true;
@@ -82,7 +68,7 @@ fn match_players(
         if match_players(players, matchings, current_player + 1) {
             return true;
         }
-        print!("match failed on {} in group {}", id, group_id)
+        print!("match failed on {} in group {}", current_player, group_id)
     }
     print!("\n");
 
@@ -94,14 +80,14 @@ fn main(){
     let mut players = Vec::new();
     let mut matchings = HashMap::new();
 
-    let ben = Player {id: 1, name: "Ben", group_id: 1};
-    let andrew = Player {id: 2, name: "Andrew", group_id: 1};
-    let nick = Player {id: 3, name: "Nick", group_id: 2};
-    let collin = Player {id: 4, name: "Collin", group_id: 2};
-    let zoe = Player {id: 5, name: "Zoe", group_id: 3};
-    let emily = Player {id: 6, name: "Emily", group_id: 3};
-    let hayden = Player {id: 7, name: "Hayden", group_id: 4};
-    let colton = Player {id: 8, name: "Colton", group_id: 4};
+    let ben = Player {name: "Ben", group_id: 1};
+    let andrew = Player {name: "Andrew", group_id: 1};
+    let nick = Player {name: "Nick", group_id: 2};
+    let collin = Player {name: "Collin", group_id: 2};
+    let zoe = Player {name: "Zoe", group_id: 3};
+    let emily = Player {name: "Emily", group_id: 3};
+    let hayden = Player {name: "Hayden", group_id: 4};
+    let colton = Player {name: "Colton", group_id: 4};
 
     players.push(ben);
     players.push(andrew);
@@ -119,17 +105,12 @@ fn main(){
         println!("Failed to sort :(");
     }
 
-    for player in &players {
-        let player_match = match matchings.get(&player.id) {
+    for (i, player) in players.iter().enumerate() {
+        let player_match = match matchings.get(&i) {
             None => panic!("Couldn't find anyone"),
             Some(i) => i
         };
 
-        let player_match = match get_player_from_id(&players, *player_match) {
-            None => panic!("Couldn't find anyone"),
-            Some(i) => &players[i],
-        };
-
-        println!("{} matched with {}", player, player_match);
+        println!("{} matched with {}", player, &players[*player_match]);
     }
 }
